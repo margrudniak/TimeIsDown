c510c34b6a575cc77324dbd5314d6892eb0f5725
margrudniak <maverickmonkey69@gmail.com>
2022-11-20 12:23:58 +0100

    feat: create basic screen Home

---
+++ src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {HomeScreenProps} from './HomeScreen.types';
+
+export const HomeScreen = ({navigation}: HomeScreenProps) => {
+  return null;
+};
---
+++ src/screens/Home/HomeScreen.types.ts
@@ -0,0 +1,5 @@
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootParamList, Screens} from 'src/navigation/screen-params';
+
+export interface HomeScreenProps
+  extends StackScreenProps<RootParamList, Screens.Home> {}
