d239d3efb149694a6e65793f418acdf252d37f4c
margrudniak <maverickmonkey69@gmail.com>
2022-11-20 13:37:23 +0100

    feat: wrap RootNavigator in NavigationContainer

--- src/navigation/root-navigator.tsx
+++ src/navigation/root-navigator.tsx
@@ -3,19 +3,22 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {RootParamList, Screens} from './screen-params';
 import {HomeScreen} from 'src/screens';
 import {color} from 'src/themes';
+import {NavigationContainer} from '@react-navigation/native';
 
 const Stack = createStackNavigator<RootParamList>();
 
 export const RootNavigator = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        animationEnabled: false,
-        cardStyle: {backgroundColor: color.transparent},
-        headerShown: false,
-      }}
-      initialRouteName={Screens.Home}>
-      <Stack.Screen component={HomeScreen} name={Screens.Home} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          animationEnabled: false,
+          cardStyle: {backgroundColor: color.transparent},
+          headerShown: false,
+        }}
+        initialRouteName={Screens.Home}>
+        <Stack.Screen component={HomeScreen} name={Screens.Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
