1fd135019848637e2ea3aa95be1bd75949c83181
margrudniak <maverickmonkey69@gmail.com>
2022-11-20 12:24:57 +0100

    feat: adjust RootNavigator on main file

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+import {RootNavigator} from 'src/navigation';
 
 const App = () => {
-  return null;
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <RootNavigator />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
