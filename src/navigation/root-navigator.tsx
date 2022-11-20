244a112e260c4ae6f590de87055070115e6ce8cd
margrudniak <maverickmonkey69@gmail.com>
2022-11-20 12:30:19 +0100

    feat: add HomeScreen to stack nav & setup configs on stack

--- src/navigation/root-navigator.tsx
+++ src/navigation/root-navigator.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {RootParamList} from './screen-params';
+import {RootParamList, Screens} from './screen-params';
+import {HomeScreen} from 'src/screens';
+import {color} from 'src/themes';
 
 const Stack = createStackNavigator<RootParamList>();
 
 export const RootNavigator = () => {
-  return null;
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        animationEnabled: false,
+        cardStyle: {backgroundColor: color.transparent},
+        headerShown: false,
+      }}
+      initialRouteName={Screens.Home}>
+      <Stack.Screen component={HomeScreen} name={Screens.Home} />
+    </Stack.Navigator>
+  );
 };
