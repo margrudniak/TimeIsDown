4298411ab008971a71e7ad16f204af276594d900
margrudniak <maverickmonkey69@gmail.com>
2022-11-21 01:25:50 +0100

    feat: add NewTimerScreen

--- src/screens/NewTimer/NewTimerScreen.style.ts
+++ src/screens/NewTimer/NewTimerScreen.style.ts
@@ -1,7 +1,40 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ViewStyle} from 'react-native';
+import {palette, spacing} from 'src/themes';
 
-interface Style {}
+interface Style {
+  screenWrapper: ViewStyle;
+  viewWrapper: ViewStyle;
+  buttonStyle: ViewStyle;
+  inputWrapper: ViewStyle;
+  inputStyle: ViewStyle;
+  wrapperConfirm: ViewStyle;
+}
 
-const styles = StyleSheet.create<Style>({});
+const styles = StyleSheet.create<Style>({
+  screenWrapper: {
+    flex: 1,
+    justifyContent: 'space-between',
+    margin: spacing[4],
+  },
+  viewWrapper: {
+    flex: 1,
+  },
+  buttonStyle: {
+    alignSelf: 'flex-start',
+  },
+  inputWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  inputStyle: {
+    height: 40,
+    borderWidth: 2,
+    borderColor: palette.black,
+    borderRadius: 2,
+  },
+  wrapperConfirm: {
+    flex: 1,
+  },
+});
 
 export default styles;
--- src/screens/NewTimer/NewTimerScreen.tsx
+++ src/screens/NewTimer/NewTimerScreen.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
-import {Button, Screen} from 'src/components';
+import React, {useState} from 'react';
+import {TextInput, View} from 'react-native';
+import {Button, Screen, Text} from 'src/components';
+import {addItem} from 'src/services';
+import styles from './NewTimerScreen.style';
 import {NewTimerProps} from './NewTimerScreen.types';
 
 export const NewTimerScreen = ({navigation}: NewTimerProps) => {
+  const [title, setTitle] = useState('');
+  const onGoBack = () => navigation.goBack();
+  const onSave = async () => {
+    const prepObj = {
+      id: new Date(),
+      title: title,
+      time: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    };
+    await addItem(prepObj);
+    navigation.goBack();
+  };
   return (
-    <Screen isLoading={false}>
-      <Button category="primary" text={'Add new timer'} />
+    <Screen isLoading={false} style={styles.screenWrapper}>
+      <View style={styles.viewWrapper}>
+        <Button
+          category="primary"
+          text={'Back'}
+          onPress={onGoBack}
+          style={styles.buttonStyle}
+        />
+        <View style={styles.inputWrapper}>
+          <Text textCategory="h3" text={'Title'} />
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Type here title"
+            onChangeText={text => setTitle(text)}
+            defaultValue={title}
+          />
+        </View>
+      </View>
+      <View style={styles.wrapperConfirm}>
+        <Button category="primary" text={'Save'} onPress={onSave} />
+      </View>
     </Screen>
   );
 };
